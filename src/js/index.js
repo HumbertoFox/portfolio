badb55a5e6ad3b7334cc02c4d901992ee14734bd
Humberto Sales <betofoxmal@gmail.com>
2023-11-14 02:55:06 -0300

    add botão top ma pag falta refatorar

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,21 @@
 const selectListAli = document.querySelectorAll('li a');
 const selectCheckdBox = document.getElementById('menu-hamburguer');
+const selectBtnTop = document.querySelector('.btn-top');
 
 for (const iterator of selectListAli) {
-    iterator.addEventListener('click', function(){
+    iterator.addEventListener('click', () => {
         selectCheckdBox.checked = false;
     });
+};
+
+document.onscroll = () => {
+    btnOcultar();
+}
+
+function btnOcultar() {
+    if(window.scrollY > 900){
+        selectBtnTop.style.display = "flex";
+    } else {
+        selectBtnTop.style.display = "none";
+    }
 };
