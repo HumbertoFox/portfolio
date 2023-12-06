9a951d8db4e1cf19392cca9b4cd085cfc345bc18
Humberto Sales <betofoxmal@gmail.com>
2023-12-05 23:20:20 -0300

    update animações @keyframes css/js inicialização

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 const selectListAli = document.querySelectorAll('li a');
 const selectCheckdBox = document.getElementById('menu-hamburguer');
 const selectBtnTop = document.querySelector('.btn-top');
+const selectAnimations = document.querySelectorAll("[data-animation]");
 
 for (const iterator of selectListAli) {
     iterator.addEventListener('click', () => {
@@ -14,3 +15,19 @@ function btnOcultar() {
     (window.scrollY > 900) ? selectBtnTop.classList.add('visivel-btn-top') 
                            : selectBtnTop.classList.remove('visivel-btn-top');
 };
+
+const animatinScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * .50;
+
+    selectAnimations.forEach((animation) => {
+        if (windowTop > animation.offsetTop) {
+            animation.classList.add("animation");
+        } else {
+            animation.classList.remove("animation");
+        };
+    });
+};
+
+window.addEventListener("scroll", () => {
+    animatinScroll();
+});
