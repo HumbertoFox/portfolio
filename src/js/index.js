d61c07299ea09e0c6dd927c88440ba56a75e6381
Humberto Ribeiro <betofoxmal@gmail.com>
2024-09-13 18:26:56 -0300

    update button skills

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,37 @@
-const selectListAli = document.querySelectorAll('li a');
-const selectCheckdBox = document.getElementById('menu-hamburguer');
-const selectBtnTop = document.querySelector('.btn-top');
+const selectListAli = document.querySelectorAll("li a");
+const selectCheckdBox = document.getElementById("menu-hamburguer");
+const selectBtnTop = document.querySelector(".btn-top");
 const selectHome = document.getElementById("home");
 const selectSectionTitle = document.getElementById("title");
 const selectAnimations = document.querySelectorAll("[data-animation]");
+const selectInformactions = document.querySelectorAll(".informacoes p");
+const selectHiddenElements = document.querySelectorAll('.informacoes.hidden');
+const selectMoreButton = document.getElementById("loadMore");
+let currentIndex = 0;
+
+selectMoreButton.addEventListener('click', () => {
+    for (let i = 0; i < 3; i++) {
+        if (currentIndex < selectHiddenElements.length) {
+            selectHiddenElements[currentIndex].classList.remove('hidden');
+            currentIndex++;
+        };
+    };
+    if (currentIndex >= selectHiddenElements.length) {
+        selectMoreButton.style.display = 'none';
+    };
+});
+
+function limitarTexto(elemento, limite) {
+    const textoOriginal = elemento.textContent;
+    if (textoOriginal.length > limite) {
+        const textoLimitado = textoOriginal.substring(0, limite) + "...";
+        elemento.textContent = textoLimitado;
+    };
+};
+
+selectInformactions.forEach((paragrafo) => {
+    limitarTexto(paragrafo, 66);
+});
 
 for (const iterator of selectListAli) {
     iterator.addEventListener('click', () => {
